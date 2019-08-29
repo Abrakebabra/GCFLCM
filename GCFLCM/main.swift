7761122ee1ba2fc553308414a7a85b22d9dc99d6
Keith Lee <lee.mingkeith@gmail.com>
2019-08-29 14:34:43 +0900

    Initial Commit

---
+++ GCFLCM/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GCFLCM
+//
+//  Created by Keith Lee on 2019/08/29.
+//  Copyright © 2019 Keith Lee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
